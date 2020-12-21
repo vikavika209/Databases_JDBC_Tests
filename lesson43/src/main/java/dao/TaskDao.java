8074fbabfe7eca839fbe9b5d38c7c19d36a15d8f
Lev Ekasov <lev.ekasov@gmail.com>
2020-12-21 22:37:48 +0100

    add tests and embedded postgres

--- lesson43/src/main/java/dao/TaskDao.java
+++ lesson43/src/main/java/dao/TaskDao.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javax.sql.DataSource;
 
 public class TaskDao {
@@ -16,20 +19,83 @@ public class TaskDao {
   }
 
   public Task save(Task task) {
-    String sql = "INSERT INTO task (title, finished, created_date) VALUES(?, ?, ?)";
-    try(Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+    // get connection
+    // create statement
+    // set params
+    // execute
+    // get id
+    // set id
+    String sql = "INSERT INTO task (title, finished, created_date) VALUES (?, ?, ?)";
+    try(
+      Connection connection = dataSource.getConnection();
+      PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+      ) {
+
       statement.setString(1, task.getTitle());
       statement.setBoolean(2, task.getFinished());
       statement.setTimestamp(3, java.sql.Timestamp.valueOf(task.getCreatedDate()));
       statement.executeUpdate();
+
       try(ResultSet resultSet = statement.getGeneratedKeys()) {
         if (resultSet.next()) {
           task.setId(resultSet.getInt(1));
         }
       }
+
     } catch (SQLException throwables) {
       throw new RuntimeException(throwables);
     }
+
     return task;
+  }
+
+  public List<Task> findAll() {
+    List<Task> tasks = new ArrayList<>();
+    String sql = "SELECT task_id, title, finished, created_date FROM task ORDER BY task_id";
+    try(Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql)
+        ) {
+
+      while(resultSet.next()) {
+        final Task task = new Task(
+          resultSet.getString(2),
+          resultSet.getBoolean(3),
+          resultSet.getTimestamp(4).toLocalDateTime()
+        );
+        task.setId(resultSet.getInt(1));
+        tasks.add(task);
+      }
+
+    } catch (SQLException throwables) {
+      throw new RuntimeException(throwables);
+    }
+
+    return tasks;
+  }
+
+  public int deleteAll() {
+
+    return 1;
+  }
+
+  public Task getById(Integer id) {
+    return null;
+  }
+
+  public List<Task> findAllNotFinished() {
+    return Collections.emptyList();
+  }
+
+  public List<Task> findNewestTasks(Integer numberOfNewestTasks) {
+    return Collections.emptyList();
+  }
+
+  public Task finishTask(Task task) {
+    return task;
+  }
+
+  public void deleteById(Integer id) {
+
   }
 }
--- lesson43/src/test/java/dao/TaskDaoTest.java
+++ lesson43/src/test/java/dao/TaskDaoTest.java
@@ -1,5 +1,6 @@
 package dao;
 
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import entity.Task;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,37 +11,43 @@ import java.time.LocalDateTime;
 import javax.sql.DataSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.postgresql.ds.PGSimpleDataSource;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TaskDaoTest {
   private TaskDao taskDao;
 
   @BeforeAll
-  public void setUp() {
-    PGSimpleDataSource dataSource = new PGSimpleDataSource();
-    dataSource.setUser("user");
-    dataSource.setPassword("password");
-    dataSource.setDatabaseName("productStar");
+  public void setUp() throws IOException {
+    DataSource dataSource = EmbeddedPostgres
+      .builder()
+      .start()
+      .getPostgresDatabase();
 
-    taskDao = new TaskDao(dataSource);
     initializeDb(dataSource);
+    taskDao = new TaskDao(dataSource);
+  }
+
+  @BeforeEach
+  public void beforeEach() {
+    taskDao.deleteAll();
   }
 
   private void initializeDb(DataSource dataSource) {
     try(InputStream inputStream = this.getClass().getResource("/initial.sql").openStream()) {
-      String initializeSql = new String(inputStream.readAllBytes());
-      try(Connection connection= dataSource.getConnection(); Statement statement = connection.createStatement()) {
-        statement.execute(initializeSql);
-      } catch (SQLException throwables) {
-        throwables.printStackTrace();
+      String sql = new String(inputStream.readAllBytes());
+      try(
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()
+      ) {
+        statement.executeUpdate(sql);
       }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
 
+    } catch (IOException | SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Test
@@ -53,36 +60,90 @@ class TaskDaoTest {
 
   @Test
   public void testFindAllReturnsAllTasks() {
+    Task firstTask = new Task("first task", false, LocalDateTime.now());
+    taskDao.save(firstTask);
 
+    Task secondTask = new Task("second task", false, LocalDateTime.now());
+    taskDao.save(secondTask);
+
+    assertThat(taskDao.findAll())
+      .hasSize(2)
+      .extracting("id")
+      .contains(firstTask.getId(), secondTask.getId());
+  }
+
+  @Test
+  public void testDeleteAllDeletesAllRowsInTasks() {
+    Task firstTask = new Task("any task", false, LocalDateTime.now());
+    taskDao.save(firstTask);
+
+    int rowsDeleted = taskDao.deleteAll();
+    assertThat(rowsDeleted).isEqualTo(1);
+
+    assertThat(taskDao.findAll()).isEmpty();
   }
 
   @Test
   public void testGetByIdReturnsCorrectTask() {
+    Task task = new Task("test task", false, LocalDateTime.now());
+    taskDao.save(task);
 
+    assertThat(taskDao.getById(task.getId()))
+      .isNotNull()
+      .extracting("id", "title", "finished", "createdDate")
+      .containsExactly(task.getId(), task.getTitle(), task.getFinished(), task.getCreatedDate());
   }
 
   @Test
-  public void testFindNotFinishedReturnsCorrectTasks() {
+  public void testFindNotFinishedReturnsCorrectTasks()  {
+    Task unfinishedTask = new Task("test task", false, LocalDateTime.now());
+    taskDao.save(unfinishedTask);
 
+    Task finishedTask = new Task("test task", false, LocalDateTime.now());
+    taskDao.save(finishedTask);
+
+    assertThat(taskDao.findAllNotFinished())
+      .singleElement()
+      .extracting("id", "title", "finished", "createdDate")
+      .containsExactly(unfinishedTask.getId(), unfinishedTask.getTitle(), unfinishedTask.getFinished(), unfinishedTask.getCreatedDate());
   }
 
   @Test
   public void testFindNewestTasksReturnsCorrectTasks() {
+    Task firstTask = new Task("first task", false, LocalDateTime.now());
+    taskDao.save(firstTask);
 
+    Task secondTask = new Task("second task", false, LocalDateTime.now());
+    taskDao.save(secondTask);
+
+    Task thirdTask = new Task("third task", false, LocalDateTime.now());
+    taskDao.save(thirdTask);
+
+    assertThat(taskDao.findNewestTasks(2))
+      .hasSize(2)
+      .extracting("id")
+      .containsExactlyInAnyOrder(secondTask.getId(), thirdTask.getId());
   }
 
   @Test
   public void testFinishSetsCorrectFlagInDb() {
+    Task task = new Task("test task", false, LocalDateTime.now());
+    taskDao.save(task);
 
-  }
-
-  @Test
-  public void deleteAllDeletesAllData() {
-
+    assertThat(taskDao.finishTask(task).getFinished()).isTrue();
+    assertThat(taskDao.getById(task.getId()).getFinished()).isTrue();
   }
 
   @Test
   public void deleteByIdDeletesOnlyNecessaryData() {
+    Task taskToDelete = new Task("first task", false, LocalDateTime.now());
+    taskDao.save(taskToDelete);
 
+    Task taskToPreserve = new Task("second task", false, LocalDateTime.now());
+    taskDao.save(taskToPreserve);
+
+    taskDao.deleteById(taskToDelete.getId());
+    assertThat(taskDao.getById(taskToDelete.getId())).isNull();
+    assertThat(taskDao.getById(taskToPreserve.getId())).isNotNull();
   }
 }
